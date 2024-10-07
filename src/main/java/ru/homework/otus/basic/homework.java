96961d7040ae0e04a1b7546afc1eb9631e2041cc
Boris Razzhivaykin <rboris93@mail.ru>
2024-10-07 23:27:41 +0300

    Добавил три раза привет мир

--- src/main/java/ru/homework/otus/basic/homework.java
+++ src/main/java/ru/homework/otus/basic/homework.java
@@ -3,5 +3,8 @@ package ru.homework.otus.basic;
 public class homework {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
